4407c78c04b792d8817adb75120dfb2712a40272
Huiran Yu <huiranyu@andrew.cmu.edu>
2024-02-13 14:01:08 -0500

    Add measure info

--- dataset/preprocessing_web.py
+++ dataset/preprocessing_web.py
@@ -41,7 +41,32 @@ def convert_midi(midi_path):
             time_sigs.append((bar_length, cur_time, prev_bar))
     if len(time_sigs) > 1:
         print(time_sigs)
-        # _ = input()
+    print("---------------------------------")
+    # Expand time signatures
+    measure_sigs = []
+    sig = time_sigs[0][0]
+    m_start = int(time_sigs[0][1])
+    for s in time_sigs[1:]:
+        end_time = s[1]
+        while m_start < end_time:
+            m_end = int(min(end_time, m_start + sig * ticks_per_beat))
+            # print(end_time, m_start, sig, ticks_per_beat)
+            measure_sigs.append([m_start, m_end, sig])
+            m_start = int(m_start + sig * ticks_per_beat)
+        m_start = min(m_start, end_time)
+        sig = s[0]
+
+    # print("loop end")
+    end_time = cur_time
+    while m_start < end_time:
+        m_end = int(min(end_time, m_start + sig * ticks_per_beat))
+        measure_sigs.append([m_start, m_end, sig])
+        m_start = int(m_start + sig * ticks_per_beat)
+    # print(m_start, m_end, sig)
+    # measure_sigs.append([m_start, cur_time, sig])
+    print(measure_sigs)
+    print(cur_time)
+    # _ = input()
     track_note_dict = {}
     for track in mid.tracks[1:]:
         if track.name == "":
@@ -51,18 +76,13 @@ def convert_midi(midi_path):
         # print(track, track.name)
         cur_time = 0
         last_time = 0
-        time_sig_p = 0
+        measure_sig_p = 0
         note_dict = {}
         track_note_list = []
         bar_note_list = []
         for msg in track:
             cur_time = cur_time + msg.time
-            while time_sig_p < len(time_sigs) - 1:
-                if time_sigs[time_sig_p + 1][1] <= cur_time:
-                    time_sig_p += 1
-                else:
-                    break
-            ts = time_sigs[time_sig_p]
+            # ts = time_sigs[time_sig_p]
             if msg.type == "note_on":
                 if msg.velocity > 0:
                     if note_dict.get(msg.note, None) is None:
@@ -72,23 +92,50 @@ def convert_midi(midi_path):
                         start_tick = note_dict[msg.note]
                         note_dict[msg.note] = None
                         end_tick = cur_time
+                        
+                        while measure_sig_p < len(measure_sigs):
+                            if measure_sigs[measure_sig_p][1] <= start_tick:
+                                measure_sig_p += 1
+                            else:
+                                break
+                        if measure_sig_p == len(measure_sigs):
+                            measure_sig_p -= 1
+                        ts = measure_sigs[measure_sig_p]
+                        # print(msg.note, start_tick, end_tick)
+                        # print(ts)
 
                         # print(ts)
                         # print(start_tick)
-                        cur_bar = floor((start_tick - ts[1]) / ticks_per_beat / ts[0]) + ts[2]
+                        cur_bar = measure_sig_p
+                        if measure_sig_p == len(measure_sigs) - 1:
+                            cur_bar += floor((start_tick - ts[0]) / ticks_per_beat / ts[2])
+                        # print(cur_bar)
+                        # print(len(track_note_list))
                         # print(cur_bar)
                         while len(track_note_list) < cur_bar:
                             track_note_list.append(bar_note_list)
                             bar_note_list = []
+                        # print(track_note_list)
 
-                        start_q = round(start_tick / ticks_per_beat * 4) / 4
-                        end_q = round(end_tick / ticks_per_beat * 4) / 4
-                        dur = end_q - start_q
+                        # start_q = round(start_tick / ticks_per_beat * 4) / 4
+                        # end_q = round(end_tick / ticks_per_beat * 4) / 4
+                        # dur = end_q - start_q
+                        # if dur == 0:
+                        #     dur = 0.25
+                        if measure_sig_p == 0:
+                            start_beat = ts[2] - (ts[1] - start_tick) / ticks_per_beat
+                            end_beat = ts[2] - (ts[1] - end_tick) / ticks_per_beat
+                        else:
+                            start_beat = ((start_tick - ts[0]) / ticks_per_beat)
+                            end_beat = ((end_tick - ts[0]) / ticks_per_beat)
+                        # print(start_beat, end_beat)
+                        start_beat_q = round(start_beat * 4) / 4
+                        dur = round(end_beat * 4) / 4 - start_beat_q
                         if dur == 0:
                             dur = 0.25
-                        start_beat = ((start_tick - ts[1]) / ticks_per_beat) % ts[0]
-                        start_beat_q = round(start_beat * 4) / 4
                         bar_note_list.append([msg.note, start_beat_q, dur])
+                        # print(bar_note_list[-1])
+                        # _ = input()
 
                         if start_beat_q not in start_time:
                             start_time[start_beat_q] = 0
@@ -100,9 +147,12 @@ def convert_midi(midi_path):
                         # if abs(start_beat - 0.375) < 0.0000001:
                         #     print(msg.note, start_beat, dur, cur_bar)
                         #     _ = input()
-                        
+        # print("fin")
+        # _ = input()
         track_note_list.append(bar_note_list)
         track_note_dict[track.name] = track_note_list
+        # print(track_note_list)
+        # _ = input()
 
 
     def idt(n1, n2):
@@ -116,7 +166,6 @@ def convert_midi(midi_path):
             return False
         return True
 
-
     for instrument in midi_data.instruments:
         if instrument.name == "":
             continue
@@ -124,9 +173,6 @@ def convert_midi(midi_path):
             continue
         track_note_list = track_note_dict[instrument.name]
         pm_notes = instrument.notes
-        # assert len(pm_notes) == sum([len(x) for x in track_note_list])
-        # print([x.pitch for x in pm_notes[:48]])
-        # print(track_note_list[:15])
 
         i = 0
         prev_n = -1
@@ -141,13 +187,21 @@ def convert_midi(midi_path):
                 note.append(n.end)
                 i += 1
                 prev_n = n
-        
 
     final_bar_count = max([len(x) for _, x in track_note_dict.items()])
     final_measure_list = [dict() for _ in range(final_bar_count)]
     for t, bars in track_note_dict.items():
         for i, b in enumerate(bars):
             final_measure_list[i][t] = b
+
+    print(len(final_measure_list))
+    print(len(measure_sigs))
+    assert len(final_measure_list) == len(measure_sigs)
+    for i, sig in enumerate(measure_sigs):
+        measure = final_measure_list[i]
+        measure["measure"] = sig[2]
+    print("i:", i)
+        
             
     return final_measure_list
 
@@ -157,17 +211,18 @@ def convert_midi(midi_path):
 
 
 if __name__ == "__main__":
-    content_dir = Path("/storageSSD/huiran/NoteTranscription/BachChorale")
-    target_dir = Path("/storageSSD/huiran/NoteTranscription/BachChorale")
-    # content_dir = Path("/media/ella/Yu/UR/datasets/BachChorale/midi_align")
+    # content_dir = Path("/storageSSD/huiran/NoteTranscription/BachChorale")
+    # target_dir = Path("/storageSSD/huiran/NoteTranscription/BachChorale")
+    content_dir = Path("/media/ella/Yu/UR/datasets/BachChorale")
     # content_dir = Path("/media/ella/Yu/UR/datasets/BachChorale/audio")
     # target_dir = Path("./test")
     # content_dir = Path("/media/ella/Yu/UR/datasets/WebChoralDataset/program_change_midi")
     # content_dir = Path("/storageSSD/huiran/NoteTranscription/WebChorale")
     # target_dir = Path("/storageSSD/huiran/NoteTranscription/WebChorale")
 
-    content_dir = Path("/media/ella/Yu/UR/datasets/WebChoralDataset")
-    target_dir = Path("./test/WebChorale")
+    # content_dir = Path("/media/ella/Yu/UR/datasets/WebChoralDataset")
+    # target_dir = Path("./test/WebChorale")
+    target_dir = Path("./test/BachChorale")
     
     mel_dir = target_dir / "mel"
     note_dir = target_dir / "note"
@@ -179,13 +234,17 @@ if __name__ == "__main__":
     # with open(content_dir / "split" / "aligned_midi" / "test.json") as fin:
     #     test_list = json.load(fin)
     # midi_list = [x for x in midi_list if x.stem in test_list]
-    
+
+    midi_list = list((content_dir / "midi_align").glob("*.mid")) # BachChorale
     # midi_list = list((content_dir / "aligned_midi").glob("*.mid"))
-    midi_list = list((content_dir / "BachChorale").glob("*.mid"))
+    # midi_list = list((content_dir / "BachChorale").glob("*.mid"))
 
     valid = 0
     # for flac in tqdm(flac_list):
     for j, midi_path in tqdm(enumerate(midi_list)):
+        if j < 0:
+            continue
+        print("j:", j)
         # midi_path = content_dir / ("%s.mid" % flac.stem)
         if not midi_path.exists():
             continue
@@ -201,10 +260,10 @@ if __name__ == "__main__":
             pickle.dump(track_note_list, fout, protocol=4)
 
             
-        # """
+        """
         # flac = content_dir / "aligned_midi" / ("%s.flac" % midi_path.stem)
         # flac = content_dir / "audio_clean" / midi_path.parts[-3] / midi_path.parts[-2] / ("%s.mp4" % midi_path.stem)
-        flac = content_dir / "BachChorale" / ("%s.flac" % midi_path.stem)
+        # flac = content_dir / "BachChorale" / ("%s.flac" % midi_path.stem)
         wave, sr = torchaudio.load(flac)
         wave_mono = wave.mean(dim=0)
         if sr != SAMPLE_RATE:
@@ -223,7 +282,7 @@ if __name__ == "__main__":
         with open(mel_dir / ("%s.pkl" % flac.stem), 'wb') as fout:
             pickle.dump(mel_spec, fout, protocol=4)
 
-        # """
+        """
 
         # break
         # _ = input()
