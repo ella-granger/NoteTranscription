70b0eb59034b6d9cdad60d1840ef4a62233a52f1
Huiran Yu <huiranyu@andrew.cmu.edu>
2024-08-20 22:11:47 -0400

    Pad mel & whisper

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -86,7 +86,20 @@ class MelDataset(torch.utils.data.Dataset):
 
         if total_length < self.seg_len:
             # pad to seg_len
-            pass
+            begin_idx = 0
+            end_idx = self.seg_len
+            begin_time = 0
+            end_time = end_idx * self.hop_len / SAMPLE_RATE
+            mel_length = mel.size(1)
+            if mel_length < self.seg_len:
+                mel = F.pad(mel, (0, self.seg_len - mel_length), value=0)
+            else:
+                mel = mel[:, :end_idx]
+            whisper_length = whisper.size(1)
+            if whisper_length < self.seg_len:
+                whisper = F.pad(whisper, (0, self.seg_len - whisper_length), value=0)
+            else:
+                whisper = whisper[:, :end_idx]
         else:
             begin_idx = np.random.randint(total_length - self.seg_len + 1)
             end_idx = begin_idx + self.seg_len
@@ -98,97 +111,98 @@ class MelDataset(torch.utils.data.Dataset):
             # begin_idx = int(begin_time * SAMPLE_RATE / self.hop_len)
             # end_idx = int(end_time * SAMPLE_RATE / self.hop_len)
 
-            cur_bar_list = []
-            start_flag = False
-            for bar in notes:
-                note_count = sum([len(v) for k, v in bar.items() if k[0] in self.voice_list])
-                time_min = 1e6
-                time_max = -1
-                # print(note_count, bar)
-                if note_count > 0 or start_flag:
-                    if note_count == 0:
-                        cur_bar_list.append(bar)
+            mel = mel[:, begin_idx:end_idx]
+            whisper = whisper[:, begin_idx:end_idx]
+        
+        cur_bar_list = []
+        start_flag = False
+        for bar in notes:
+            note_count = sum([len(v) for k, v in bar.items() if k[0] in self.voice_list])
+            time_min = 1e6
+            time_max = -1
+            # print(note_count, bar)
+            if note_count > 0 or start_flag:
+                if note_count == 0:
+                    cur_bar_list.append(bar)
+                    continue
+                for k, v in bar.items():
+                    if k[0] not in self.voice_list:
                         continue
-                    for k, v in bar.items():
-                        if k[0] not in self.voice_list:
-                            continue
-                        for note in v:
-                            if note[-1] > time_max:
-                                time_max = note[-1]
-                            if note[-2] < time_min:
-                                time_min = note[-2]
-                    if time_min < end_time and time_max > begin_time:
-                        cur_bar_list.append(bar)
-                        start_flag = True
-                    if time_min > end_time:
-                        break
-            # print(begin_time, end_time)
-            # print(cur_bar_list)
-            token = []
-            if "S" in self.train_mode:
-                start = []
-                dur = []
-            if "T" in self.train_mode:
-                start_t = []
-                end = []
+                    for note in v:
+                        if note[-1] > time_max:
+                            time_max = note[-1]
+                        if note[-2] < time_min:
+                            time_min = note[-2]
+                if time_min < end_time and time_max > begin_time:
+                    cur_bar_list.append(bar)
+                    start_flag = True
+                if time_min > end_time:
+                    break
+        # print(begin_time, end_time)
+        # print(cur_bar_list)
+        token = []
+        if "S" in self.train_mode:
+            start = []
+            dur = []
+        if "T" in self.train_mode:
+            start_t = []
+            end = []
 
-            for bar in cur_bar_list:
-                full = False
-                sig = bar["measure"]
-                max_time = 0
-                note_list = []
-                for part in self.voice_list:
-                    part_list = []
-                    last_list = []
-                    for k, v in bar.items():
-                        if part in k:
-                            part_list += v
-                            if len(v) > 0:
-                                last_list.append(v[-1])
-                    part_list = sorted(part_list, key=lambda x: (x[3], -x[0]))
-                    # print("--------------------------------")
-                    # print(part)
-                    # print(part_list)
-                    # print(last_list)
+        for bar in cur_bar_list:
+            full = False
+            sig = bar["measure"]
+            max_time = 0
+            note_list = []
+            for part in self.voice_list:
+                part_list = []
+                last_list = []
+                for k, v in bar.items():
+                    if part in k:
+                        part_list += v
+                        if len(v) > 0:
+                            last_list.append(v[-1])
+                part_list = sorted(part_list, key=lambda x: (x[3], -x[0]))
+                # print("--------------------------------")
+                # print(part)
+                # print(part_list)
+                # print(last_list)
 
-                    last_count = 0
-                    for idx, note in enumerate(part_list):
-                        if note[3] < end_time and note[4] >= begin_time:
-                            new_note = deepcopy(note)
-                            new_note[3] = max(note[3], begin_time)
-                            new_note[4] = min(note[4], end_time)
-                            note_list.append(tuple(new_note))
-                            if note in last_list:
-                                full = True
+                last_count = 0
+                for idx, note in enumerate(part_list):
+                    if note[3] < end_time and note[4] >= begin_time:
+                        new_note = deepcopy(note)
+                        new_note[3] = max(note[3], begin_time)
+                        new_note[4] = min(note[4], end_time)
+                        note_list.append(tuple(new_note))
+                        if note in last_list:
+                            full = True
 
-                # print(note_list)
-                note_list = list(set(note_list))
-                note_list = sorted(note_list, key=lambda x:(x[3], -x[0]))
+            # print(note_list)
+            note_list = list(set(note_list))
+            note_list = sorted(note_list, key=lambda x:(x[3], -x[0]))
 
-                for note in note_list:
-                    token.append(note[0])
-                    if "S" in self.train_mode:
-                        start.append(note[1])
-                        dur.append(note[2])
+            for note in note_list:
+                token.append(note[0])
+                if "S" in self.train_mode:
+                    start.append(note[1])
+                    dur.append(note[2])
+                if "T" in self.train_mode:
+                    start_t.append(note[3])
+                    end.append(note[4])
+                    if note[4] > max_time:
+                        max_time = note[4]
+            if full:
+                if "S" in self.train_mode:
+                    token.append(0)
+                    start.append(sig)
+                    dur.append(0.0)
                     if "T" in self.train_mode:
-                        start_t.append(note[3])
-                        end.append(note[4])
-                        if note[4] > max_time:
-                            max_time = note[4]
-                if full:
-                    if "S" in self.train_mode:
-                        token.append(0)
-                        start.append(sig)
-                        dur.append(0.0)
-                        if "T" in self.train_mode:
-                            start_t.append(max_time)
-                            end.append(max_time)
+                        start_t.append(max_time)
+                        end.append(max_time)
             # print(token)
             # print(start)
             # print(dur)
             
-            mel = mel[:, begin_idx:end_idx]
-            whisper = whisper[:, begin_idx:end_idx]
             token.insert(0, MAX_MIDI+1)
             token.append(MAX_MIDI+2)
             token = torch.LongTensor(token)
